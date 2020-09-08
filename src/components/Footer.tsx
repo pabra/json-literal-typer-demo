176d71d77e29d033f4574dcdf0c2d3cdd87e3bf0
pabra <patrick+github-commit-2020@braune.xyz>
2020-09-08 12:14:26 +0200

    update footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,21 +5,27 @@ import styles from './Footer.module.css';
 
 const Footer: React.FC = () => (
   <div className={styles.root}>
-    <div className={styles.box}>demo version: {demoPJ.version}</div>
     <div className={styles.box}>
+      demo version: {demoPJ.version} (source on{' '}
+      <a
+        href={demoPJ.repository.url.replace(/(:?^git\+)i?/, '')}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+      )
+    </div>
+    <div className={styles.box}>
+      JSON-literal-typer version: {typerPj.version} (source on{' '}
       <a
         href={typerPj.repository.url.replace(/(:?^git\+)i?/, '')}
         target="_blank"
         rel="noopener noreferrer"
       >
-        JSON-literal-typer version: {typerPj.version}
-      </a>
-    </div>
-    <div className={styles.box}>
-      source on{' '}
-      <a href={demoPJ.homepage} target="_blank" rel="noopener noreferrer">
         GitHub
       </a>
+      )
     </div>
   </div>
 );
